5a6f28fb5d03a3f82d82944bd250ee9564d05dc2
Kağan Kılıç <kaganklc44@gmail.com>
2022-11-04 00:00:03 +0300

    chartbar

--- lib/widgets/kart.dart
+++ lib/widgets/kart.dart
@@ -24,11 +24,9 @@ class Kart extends StatelessWidget {
             totalSum += recentTransactions[i].amount;
           }
         }
-        print(DateFormat.E().format(weekDay));
-        print(totalSum);
 
         return {
-          'day': DateFormat.E().format(weekDay),
+          'day': DateFormat.E().format(weekDay).substring(0, 1),
           'amount': totalSum,
         };
       },
@@ -42,7 +40,9 @@ class Kart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
---
+++ lib/widgets/kart_board.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:real_app_by_kagan/widgets/kart.dart';
+
+class KartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  KartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromARGB(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColor),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
